d62e0b0d6bd565d56fa5abd8916ea9bcf62802ec
Tyler Bisson <tyler.bisson@slalom.com>
2023-04-05 14:14:19 -0400

    Initialize capacitor project

---
+++ capacitor.config.ts
@@ -0,0 +1,10 @@
+import { CapacitorConfig } from '@capacitor/cli';
+
+const config: CapacitorConfig = {
+  appId: 'ionic.djbuddy2',
+  appName: 'djbuddy2',
+  webDir: 'www',
+  bundledWebRuntime: false
+};
+
+export default config;
